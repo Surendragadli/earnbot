605158ade5cd9f32496590045ab2efda0ebc10f9
Surendragadli <gadlisk57@gmail.com>
2026-05-16 19:50:26 +0530

    Update bot token and modify polling method
    Updated bot token and removed unnecessary print statement.

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import telebot
 
-TOKEN = "8894488238:AAFBcqMsg47CU3wRengom-zGWB37t3P29Xs"
+TOKEN = "8894488238:AAFom771RgGEoPb_Wk8jBlxPtIBPW3fu8_g"
 
 bot = telebot.TeleBot(TOKEN)
 
@@ -8,5 +8,4 @@ bot = telebot.TeleBot(TOKEN)
 def start(message):
     bot.reply_to(message, "Bot Working ✅")
 
-print("Bot Running...")
-bot.infinity_polling(skip_pending=True, none_stop=True)
+bot.infinity_polling(skip_pending=True)
